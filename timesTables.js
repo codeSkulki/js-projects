823a0664bb9aa68be8310e7768ac669ca6ea5f66
Dev <63323219+codeSkulki@users.noreply.github.com>
2024-07-31 16:40:15 +0000

    adjusted files and setup foundation

--- timesTables.js
+++ timesTables.js
@@ -1,1 +1,11 @@
+// this function takes a number and shows you the individual results of multiplying it by 1 through 10 
+function timesTables(num){
+    for (let i = 1; i <=10; i++){
+        console.log(`${num} x ${i} = ${num*i}`);
+    }
+}
 
+// this is an example of using it
+timesTables(9);
+
+//this is where I will write a simple webpage that allows users to input their own number from a page and have a simple UI/display
