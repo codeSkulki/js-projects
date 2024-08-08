39e32a23a7b2d08c5c8c8f2422eb2fb81014d096
Dev <63323219+codeSkulki@users.noreply.github.com>
2024-08-08 23:02:10 +0000

    setting up length converter layout notes

---
+++ LengthConverter/script.js
@@ -0,0 +1,3 @@
+//this function takes a length in inches and converts it to feet+inches
+
+//something like: num%12 remove(remainder) = feet (12*remainder=additional inches) result = (feet) & (inches)
